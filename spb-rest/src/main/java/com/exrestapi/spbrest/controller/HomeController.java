d784bf3a5bc0f47ba56120dd2fbe9b5354e7c001
서현석 <71122744+aodhzld45@users.noreply.github.com>
2024-05-23 10:30:42 +0900

    2024-05-23 SpringBoot RestApi 최초 commit

---
+++ spb-rest/src/main/java/com/exrestapi/spbrest/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.exrestapi.spbrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "{home}";
+    }
+
+    @GetMapping("/home2")
+    public String home2() {
+        return "{home2}";
+    }
+
+
+}
---
+++ spb-rest/src/test/java/com/exrestapi/spbrest/SpbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exrestapi.spbrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
